30d7812c3a0483a0f98e79bde1f9cf2d31842e2e
Fernando <sueli2011>
2015-06-10 22:26:44 -0300

    adicionando projeto

---
+++ js/src/main/java/br/com/js/Application.java
@@ -0,0 +1,14 @@
+package br.com.js;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+
+@EnableConfigurationProperties
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
+}
