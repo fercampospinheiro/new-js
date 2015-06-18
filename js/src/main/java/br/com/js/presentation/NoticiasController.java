3fbdef08c579763c5d5fcb980f556ba890120f07
Fernando <sueli2011>
2015-06-17 23:59:32 -0300

    atualizacao para teste no heroku

--- js/src/main/java/br/com/js/presentation/NoticiasController.java
+++ js/src/main/java/br/com/js/presentation/NoticiasController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class NoticiasController {
 
-    @RequestMapping("/noticias")
+    @RequestMapping("noticias")
     public String carregaNoticias(){
 
         return "noticias";
